972ceec01bb0b861f896601f91bfb157340a21eb
Deepanraj22 <deepanraj.kk2023@vitstudent.ac.in>
2025-10-17 16:11:21 +0530

    Update q7.php

--- Assignment10/q7.php
+++ Assignment10/q7.php
@@ -1,10 +1,9 @@
 <?php
-// Q7: Add Text on an Image (watermark)
 $srcFile = __DIR__ . '/sample.jpg';
 if(!file_exists($srcFile)){
-    header('Content-Type: text/plain');
-    echo 'Place a sample.jpg in the same folder to run this demo.';
-    exit;
+header('Content-Type: text/plain');
+echo 'Place a sample.jpg in the same folder to run this demo.';
+exit;
 }
 $img = imagecreatefromjpeg($srcFile);
 $color = imagecolorallocate($img, 0, 0, 255);
