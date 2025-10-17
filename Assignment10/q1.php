2a823d937d7ac520fd2f4773124ccf007c118d91
Deepanraj22 <deepanraj.kk2023@vitstudent.ac.in>
2025-10-17 16:09:09 +0530

    Update q1.php

--- Assignment10/q1.php
+++ Assignment10/q1.php
@@ -1,11 +1,10 @@
 <?php
-// Q1: Cookie-Based Visit Counter
 if(isset($_COOKIE['visits'])) {
-    $count = intval($_COOKIE['visits']) + 1;
+$count = intval($_COOKIE['visits']) + 1;
 } else {
-    $count = 1;
+$count = 1;
 }
-setcookie('visits', $count, time() + 3600); // cookie valid for 1 hour
+setcookie('visits', $count, time() + 3600);
 ?>
 <!doctype html>
 <html><body>
